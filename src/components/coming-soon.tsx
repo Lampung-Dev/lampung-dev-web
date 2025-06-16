d4ee718d4410845e00e6e9e223a70c7d825f2280
Muhamad Dani Ramanda <muhamaddani3004@gmail.com>
2025-06-16 18:02:19 +0700

    test trigger

--- src/components/coming-soon.tsx
+++ src/components/coming-soon.tsx
@@ -5,7 +5,7 @@ export default function ComingSoon() {
         <div className='w-full h-full flex justify-center items-center'>
             <div className="flex flex-col items-center justify-center gap-6 p-6 md:p-10 rounded-xl">
                 <div className="flex w-full flex-col gap-6 items-center justify-center">
-                    <SparklesText text="Coming Soon"
+                    <SparklesText text="Coming Soon Github Actions"
                         className="text-4xl md:text-6xl lg:text-7xl font-bold"
                     />
                 </div>
